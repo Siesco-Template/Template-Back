9ba2de0f59cb86b3d756364c4ef86764072a725c
Melek.H <melek.h@siesco.az>
2025-08-11 12:55:32 +0400

    .

---
+++ Authentication/Auth.Core/Entities/Detail.cs
@@ -0,0 +1,17 @@
+﻿using AFMISMain.Core.Entities;
+
+namespace Auth.Core.Entities
+{
+    public class Detail : BaseEntity
+    {
+        public string Title { get; set; }
+        public int Code { get; set; }
+
+        public Guid? ParentDetailId { get; set; }
+        public Detail? ParentDetail { get; set; }
+
+        public ICollection<Detail>? Childrens { get; set; }
+
+        public ICollection<ReportDetail>? ReportDetails { get; set; }
+    }
+}
---
+++ Authentication/Auth.Core/Entities/Report.cs
@@ -0,0 +1,24 @@
+﻿using AFMISMain.Core.Entities;
+using SharedLibrary.Enums;
+
+namespace Auth.Core.Entities
+{
+    public class Report : BaseEntity
+    {
+        public string Number { get; set; }
+        public DateTime CompileDate { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Term Term { get; set; }
+        public string? ClassificationCode1 { get; set; }
+        public string? ClassificationCode2 { get; set; }
+
+        public string? FuntionalClassificationCode { get; set; }
+        public ReportStatus ReportStatus { get; set; }
+
+        public Guid OrganizationId { get; set; }
+        public Organization Organization { get; set; }
+
+        public ICollection<ReportDetail>? ReportDetails { get; set; }
+    }
+}
